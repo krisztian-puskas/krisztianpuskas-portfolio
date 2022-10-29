67d53c880d9b8a016664bc7bb90d0cf29c8b5025
krisztian-puskas <116976890+krisztian-puskas@users.noreply.github.com>
2022-10-29 22:06:59 +0200

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementById('burger')
-const nav = document.getElementById('nav')
-
-burger.addEventListener('click',function (event){
-    nav.classList.toggle('menu-active')
-})
